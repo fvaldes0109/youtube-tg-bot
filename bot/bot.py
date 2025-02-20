0c4343fc114740ff454f5c5f9ed7bd5c702f73af
fvaldes0109 <fvaldes0109@gmail.com>
2025-02-20 17:26:45 +0100

    Polling

--- bot/bot.py
+++ bot/bot.py
@@ -3,12 +3,18 @@ import os
 from telegram.ext import ApplicationBuilder
 
 from routes.api import init_routes
-    
+import logging
+
 if __name__ == '__main__':
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
     
     TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
     app = ApplicationBuilder().token(TOKEN).build()
     
     init_routes(app)
 
-    app.run_polling() #webhook() for webhook mode
+    app.run_polling()
